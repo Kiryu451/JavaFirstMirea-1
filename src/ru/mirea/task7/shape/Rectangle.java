6fb8e894d320a457bef6eae18207e4c39665bfdc
Кирилл <90781461+Kiryu451@users.noreply.github.com>
2021-10-14 14:14:55 +0300

    Create Rectangle.java

---
+++ src/ru/mirea/task7/shape/Rectangle.java
@@ -0,0 +1,56 @@
+package ru.mirea.task7.shape;
+
+public class Rectangle extends Shape{
+    protected double width;
+    protected double length;
+
+    public Rectangle(){
+        width = 7.8;
+        length = 5.4;
+    }
+
+    public Rectangle(double width, double length){
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled){
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return width * 2 + length * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
