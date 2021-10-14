04df000dd17ddd165dc13ac87976f6ad2c816e17
Кирилл <90781461+Kiryu451@users.noreply.github.com>
2021-10-14 14:20:33 +0300

    Create Three.java

---
+++ src/ru/mirea/task10/Three.java
@@ -0,0 +1,21 @@
+package ru.mirea.task10;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Three {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if(a <= b){
+            for (int i = a; i <= b; i++)
+                System.out.print(i + " ");
+        }
+        else{
+            for (int i = b; i <= a; i++)
+                System.out.print(i + " ");
+        }
+    }
+}
