36265b00c920155ac1df25eff0494a05b993aa79
Кирилл <90781461+Kiryu451@users.noreply.github.com>
2021-10-14 14:05:45 +0300

    Create Dog.java

---
+++ src/ru/mirea/task5/Dog.java
@@ -0,0 +1,13 @@
+package ru.mirea.task5;
+
+abstract class Dog {
+    private String name;
+
+    public Dog(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
